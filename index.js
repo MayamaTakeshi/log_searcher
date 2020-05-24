b3571a3e4874714aa362e7455a862335ef38fd28
MayamaTakeshi <mayamatakeshi@gmail.co.jp>
2020-05-24 12:50:35 +0900

    Added code to handle exception

--- index.js
+++ index.js
@@ -10,33 +10,37 @@ const _ = require('lodash')
 const app = express()
 app.use(bodyParser.json())
 
-app.post("/search", async (req, res) => {
-	console.log(req.body)
+app.post("/search", async (req, res, next) => {
+	try {
+		console.log(req.body)
+		var start = m(req.body.start).unix() * 1000
+		var end = m(req.body.end).unix() * 1000 + 999
 
-	var start = m(req.body.start).unix() * 1000
-	var end = m(req.body.end).unix() * 1000 + 999
+		//console.log(start, end)
 
-	//console.log(start, end)
+		var pattern = req.body.pattern
+		var is_regex = req.body.is_regex
+		if(is_regex) {
+			pattern = new RegExp(pattern)
+		}	
 
-	var pattern = req.body.pattern
-	var is_regex = req.body.is_regex
-	if(is_regex) {
-		pattern = new RegExp(pattern)
-	}	
+		var folders = _.map(req.body.folders, (folder) => config.base_dir + "/" + folder)
 
-	var folders = _.map(req.body.folders, (folder) => config.base_dir + "/" + folder)
+		var result = await fs.filter_folders(folders, start, end, pattern, is_regex)
+		//console.log(result)
 
-	var result = await fs.filter_folders(folders, start, end, pattern, is_regex)
-	//console.log(result)
+		res.status(200)
 
-	res.status(200)
+		for (const line of result) {
+			res.write(line)
+			res.write("\n")
+		}
 
-	for (const line of result) {
-		res.write(line)
-		res.write("\n")
+		res.end()
+	} catch (error) {
+		console.log(error)
+		return next(error)
 	}
-
-	res.end()
 })
 
 app.listen(config.local_port, config.local_ip, () => console.log(`log_searcher listening at ${config.local_port}`))
