e1d9b4e053fae7d5527d0920627dad786916d6a7
root <root@IPX078>
2020-05-24 12:14:25 +0900

    Changed reply from json to plain text

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ app.use(bodyParser.json())
 
 app.post("/search", async (req, res) => {
 	console.log(req.body)
+
 	var start = m(req.body.start).unix() * 1000
 	var end = m(req.body.end).unix() * 1000 + 999
-	console.log(start, end)
+
+	//console.log(start, end)
+
 	var pattern = req.body.pattern
 	var is_regex = req.body.is_regex
 	if(is_regex) {
@@ -22,9 +25,16 @@ app.post("/search", async (req, res) => {
 	var folders = req.body.folders
 
 	var result = await fs.filter_folders(folders, start, end, pattern, is_regex)
-	console.log(result)
+	//console.log(result)
 
-	res.status(200).send(result)
+	res.status(200)
+
+	for (const line of result) {
+		res.write(line)
+		res.write("\n")
+	}
+
+	res.end()
 })
 
 app.listen(config.local_port, config.local_ip, () => console.log(`log_searcher listening at ${config.local_port}`))
