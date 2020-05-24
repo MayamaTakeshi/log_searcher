2087bbefbf03521afc3b9150854053699e0335b6
root <root@IPX078>
2020-05-24 12:04:01 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const m = require('moment')
+const config = require('config')
+
+const fs = require('./lib/file_searcher.js')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.post("/search", async (req, res) => {
+	console.log(req.body)
+	var start = m(req.body.start).unix() * 1000
+	var end = m(req.body.end).unix() * 1000 + 999
+	console.log(start, end)
+	var pattern = req.body.pattern
+	var is_regex = req.body.is_regex
+	if(is_regex) {
+		pattern = new RegExp(pattern)
+	}	
+
+	var folders = req.body.folders
+
+	var result = await fs.filter_folders(folders, start, end, pattern, is_regex)
+	console.log(result)
+
+	res.status(200).send(result)
+})
+
+app.listen(config.local_port, config.local_ip, () => console.log(`log_searcher listening at ${config.local_port}`))
